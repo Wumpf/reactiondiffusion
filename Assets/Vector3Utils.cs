b5393515fe78e518300d896b5d18456512386a4b
Andreas Reich <r_andreas2@web.de>
2018-07-21 17:49:48 +0200

    Added missing Vector3Utils file

---
+++ Assets/Vector3Utils.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+static class Vector3Utils
+{
+    public static Vector3 From(float f)
+    {
+        return new Vector3(f, f, f);
+    }
+
+    public static Vector3 From(Vector2 v, float z = 0.0f)
+    {
+        return new Vector3(v.x, v.y, z);
+    }
+}
